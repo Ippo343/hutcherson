33d2f99ddc18c441e952469a003a77a3227ed933
Michele Ippolito <m.ippolito90@gmail.com>
2018-05-11 22:29:56 +0200

    Basic ability to react to push and PRs

--- hutcherson.py
+++ hutcherson.py
@@ -1,38 +1,84 @@
 #!/usr/bin/env python3
+# -*- coding: utf8 -*-
 
-from http.server import BaseHTTPRequestHandler, HTTPServer
-import logging
 import argparse
-
+import json
+import logging
 import sys
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+
+def is_pr_event(data):
+    return 'pull_request' in data
+
+
+def is_push_event(data):
+    return 'ref' in data and 'before' in data and 'after' in data
 
 
 class RequestHandler(BaseHTTPRequestHandler):
     options = None
 
-    def _set_response(self, response_code=200):
+    def _set_response(self, response_code=200, message=None):
         self.send_response(response_code)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
 
+        if message:
+            self.wfile.write(message)
+
     def do_GET(self):
         logging.info("GET request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
-        self._set_response()
-        self.wfile.write("GET request for {}\n".format(self.path).encode('utf-8'))
+        self._set_response(
+            response_code=200,
+            message="GET request for {}\n".format(self.path).encode('utf-8')
+        )
 
     def do_POST(self):
         content_length = int(self.headers['Content-Length'])  # <--- Gets the size of data
-        post_data = self.rfile.read(content_length).decode('utf-8')  # <--- Gets the data itself
         logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\n",
                      str(self.path), str(self.headers))
 
         secret = self.headers.get(self.options.security_header)
         if not secret or secret != self.options.secret:
-            self._set_response(403)
-            self.wfile.write("403: forbidden\n".encode('utf-8'))
+            self._set_response(
+                response_code=403,
+                message="403: forbidden\n".encode('utf-8')
+            )
+            return
+
+        post_data = self.rfile.read(content_length).decode('utf-8')  # <--- Gets the data itself
+        post_data = json.loads(post_data)
+
+        if is_pr_event(post_data):
+            self.handle_pull_request(post_data)
+        elif is_push_event(post_data):
+            self.handle_push(post_data)
         else:
-            self._set_response()
-            self.wfile.write("Your data is being processed (meh)\n".encode('utf-8'))
+            self._set_response(
+                response_code=406,
+                message="Not recognized\n".encode('utf-8'))
+
+    def handle_pull_request(self, post_data):
+
+        pr_data = post_data['pull_request']
+        logging.info(
+            "Event for PR #{} from {} to {}".format(
+                post_data['number'],
+                pr_data['head']['label'],
+                pr_data['base']['label'],
+            )
+        )
+
+    def handle_push(self, post_data):
+
+        logging.info(
+            "Push event for {} ({} -> {})".format(
+                post_data['ref'],
+                post_data['before'],
+                post_data['after'],
+            )
+        )
 
 
 def run(options):
