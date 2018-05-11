48cb3610d86fe76526e21e8f998ff9e796973093
Michele Ippolito <m.ippolito90@gmail.com>
2018-05-11 21:58:47 +0200

    Very basic test of token authentication
    Verifies that the given header actually matches the expected
    secret token, and returns 403 if it doesn't.

--- hutcherson.py
+++ hutcherson.py
@@ -2,11 +2,16 @@
 
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import logging
+import argparse
+
+import sys
 
 
-class S(BaseHTTPRequestHandler):
-    def _set_response(self):
-        self.send_response(200)
+class RequestHandler(BaseHTTPRequestHandler):
+    options = None
+
+    def _set_response(self, response_code=200):
+        self.send_response(response_code)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
 
@@ -17,31 +22,41 @@ class S(BaseHTTPRequestHandler):
 
     def do_POST(self):
         content_length = int(self.headers['Content-Length'])  # <--- Gets the size of data
-        post_data = self.rfile.read(content_length)  # <--- Gets the data itself
-        logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
-                     str(self.path), str(self.headers), post_data.decode('utf-8'))
+        post_data = self.rfile.read(content_length).decode('utf-8')  # <--- Gets the data itself
+        logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\n",
+                     str(self.path), str(self.headers))
 
-        self._set_response()
-        self.wfile.write("POST request for {}\n".format(self.path).encode('utf-8'))
+        secret = self.headers.get(self.options.security_header)
+        if not secret or secret != self.options.secret:
+            self._set_response(403)
+            self.wfile.write("403: forbidden\n".encode('utf-8'))
+        else:
+            self._set_response()
+            self.wfile.write("Your data is being processed (meh)\n".encode('utf-8'))
 
 
-def run(server_class=HTTPServer, handler_class=S, port=8080):
-    logging.basicConfig(level=logging.INFO)
-    server_address = ('', port)
-    httpd = server_class(server_address, handler_class)
+def run(options):
+    server_address = ('', options.port)
+    RequestHandler.options = options
+    httpd = HTTPServer(server_address, RequestHandler)
+
     logging.info('Starting httpd...\n')
     try:
         httpd.serve_forever()
     except KeyboardInterrupt:
         pass
+
     httpd.server_close()
     logging.info('Stopping httpd...\n')
 
 
 if __name__ == '__main__':
-    from sys import argv
+    logging.basicConfig(level=logging.INFO)
 
-    if len(argv) == 2:
-        run(port=int(argv[1]))
-    else:
-        run()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--port', type=int, default=48464)
+    parser.add_argument('--security-header', type=str, default='X-Hub-Signature')
+    parser.add_argument('--secret', type=str, required=True)
+    args = parser.parse_args(sys.argv[1:])
+
+    run(args)
